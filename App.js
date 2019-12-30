207050b5feafbe1be7bad5d186de130cf5479a60
Kevin G <kevin@myplaceonline.com>
2019-12-29 17:50:22 -0800

    Create basic reproduction

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Text, View, FlatList } from 'react-native';
+import { GiftedChat, Message } from 'react-native-gifted-chat';
 
-export default function App() {
+function Item({ title }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ backgroundColor: 'lightgrey', padding: 5 }}>
+      <Text style={{ fontSize: 20 }}>{title}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <GiftedChat
+        messages={[...Array(3).keys()].map(i => {
+          return {
+            _id: i,
+            text: 'Hello World ' + i,
+            createdAt: new Date(),
+            user: {
+              _id: 1,
+              name: 'User1',
+            },
+          };
+        })}
+        user={{
+          _id: 1,
+        }}
+        renderMessage={(props) => {
+          return (
+            <View>
+              <Message {... props} />
+              <FlatList
+                style={{ borderWidth: 1, borderColor: "black", height: 200, margin: 30 }}
+                nestedScrollEnabled={true}
+                data={[...Array(10).keys()].map(i => {
+                  return { id: '' + i, title: 'FlatList item ' + i };
+                })}
+                renderItem={({ item }) => <Item title={item.title} />}
+              />
+            </View>
+          );
+        }}
+        listViewProps={{
+          nestedScrollEnabled: true
+        }}
+      />
+    </KeyboardAvoidingView>
+  );
+}
